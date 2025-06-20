fef6a9c5449551c8369494aa32968f80aad0f9b6
leynox64 <chinoidlacaille0@gmail.com>
2025-06-20 20:44:41 +0200

    target: a54x: patches: fixup

--- target/a54x/patches/midas/customize.sh
+++ target/a54x/patches/midas/customize.sh
@@ -1,6 +1,5 @@
 # Fix MIDAS model detection
-sed -i "s/$SOURCE_CODENAME/dummy/g" "$WORK_DIR/vendor/etc/midas/midas_config.json"
-sed -i "s/a54x/$SOURCE_CODENAME/g" "$WORK_DIR/vendor/etc/midas/midas_config.json"
+sed -i "s/$SOURCE_CODENAME/a54x/g" "$WORK_DIR/vendor/etc/midas/midas_config.json"
 
 # Add A54 MIDAS libraries
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libmidas_core.camera.samsung.so"
