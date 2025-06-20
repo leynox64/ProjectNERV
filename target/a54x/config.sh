f609f8a9bdc35e2ee1e6074123ed0d920589884f
leynox64 <chinoidlacaille0@gmail.com>
2025-06-20 19:07:43 +0200

    target: a54x: convert to unix format

---
+++ target/a54x/config.sh
@@ -0,0 +1,53 @@
+#
+# Copyright (C) 2024 BlackMesa123
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Device configuration file for Galaxy A54 5G (a54x)
+TARGET_NAME="Galaxy A54 5G"
+TARGET_CODENAME="a54x"
+TARGET_FIRMWARE="SM-A546E/XSG/350090711234564"
+TARGET_EXTRA_FIRMWARES=("")
+TARGET_API_LEVEL=35
+TARGET_PRODUCT_FIRST_API_LEVEL=33
+TARGET_VENDOR_API_LEVEL=33
+TARGET_SINGLE_SYSTEM_IMAGE="essi"
+TARGET_OS_FILE_SYSTEM="erofs"
+TARGET_SUPER_GROUP_NAME="group_basic"
+TARGET_SUPER_PARTITION_SIZE=11429478400
+TARGET_SUPER_GROUP_SIZE=11425284096
+TARGET_HAS_SYSTEM_EXT=false
+
+# SEC Product Feature
+TARGET_AUTO_BRIGHTNESS_TYPE="5"
+TARGET_DVFS_CONFIG_NAME="dvfs_policy_s5e8835_xx"
+TARGET_FP_SENSOR_CONFIG="google_touch_display_optical,settings=3,no_delay_in_screen_off,transition_effect_on"
+TARGET_HAS_MASS_CAMERA_APP=true
+TARGET_HAS_QHD_DISPLAY=false
+TARGET_HFR_MODE="2"
+TARGET_HFR_SUPPORTED_REFRESH_RATE="60,120"
+TARGET_HFR_DEFAULT_REFRESH_RATE="120"
+TARGET_IS_ESIM_SUPPORTED=false
+TARGET_HAS_HW_MDNIE=false
+TARGET_MDNIE_SUPPORTED_MODES="37905"
+TARGET_MDNIE_WEAKNESS_SOLUTION_FUNCTION="0"
+TARGET_AUDIO_SUPPORT_DUAL_SPEAKER=true
+TARGET_AUDIO_SUPPORT_ACH_RINGTONE=false
+TARGET_AUDIO_SUPPORT_VIRTUAL_VIBRATION=false
+TARGET_MULTI_MIC_MANAGER_VERSION="07010"
+TARGET_SSRM_CONFIG_NAME="siop_a54x_s5e8835"
+TARGET_SUPPORT_CUTOUT_PROTECTION=false
+TARGET_HFR_SEAMLESS_BRT="89,91"
+TARGET_HFR_SEAMLESS_LUX="200,2500"
